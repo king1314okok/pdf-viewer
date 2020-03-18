c29f820334aa658937fef8ecd55ed2334618996b
king1314okok <759046694@qq.com>
2020-03-18 18:01:20 +0800

    修改图标和导航守卫title

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,28 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      redirect: '/pdfPage'
+      redirect: '/pdfPage',
+      meta: {
+        title: '思特威文档浏览系统'
+      }
     },
     {
       path: '/pdfPage',
       name: 'PdfPage',
-      component: resolve => require(['@/pages/PdfPage'], resolve)
+      component: resolve => require(['@/pages/PdfPage'], resolve),
+      meta: {
+        title: '思特威文档浏览系统'
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  // 路由发生变化修改页面title
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
 })
 
 export default router
